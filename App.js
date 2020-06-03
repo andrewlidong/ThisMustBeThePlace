d0b3951db2b277482315c5ffab378dbddf0f8ff9
Andrew Dong <andrewdong1994@gmail.com>
2020-06-03 17:31:50 -0400

    Fix placeholder bug by adding text default to images

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
@@ -14,7 +13,9 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-  <Provider store={store}>
-    return <PlacesNavigator />;
-  </Provider>;
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
 }
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
+import PlaceItem from '../components/PlaceItem';
 
 const PlacesListScreen = (props) => {
+  // @ts-ignore
+  const places = useSelector((state) => state.places.places);
+
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={'placeholder'}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate('PlaceDetail', {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+        />
+      )}
+    />
   );
 };
 
